3226fb0144a888be35c0c2eeac347f3b87b652d2
Julien Delasoie <julien.delasoie@epfl.ch>
2018-02-14 20:00:56 +0100

    Remove Selenium tests

--- infoscience_exports/exports/test/test_selenium.py
+++ infoscience_exports/exports/test/test_selenium.py
@@ -1,25 +1,27 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.urls import reverse
-from selenium.webdriver.firefox.webdriver import WebDriver
-from exports.models import Export
-
-
-class MySeleniumTests(StaticLiveServerTestCase):
-    @classmethod
-    def setUpClass(cls):
-        super(MySeleniumTests, cls).setUpClass()
-        cls.selenium = WebDriver()
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.selenium.quit()
-        super(MySeleniumTests, cls).tearDownClass()
-
-    def test_create(self):
-        name = 'name1'
-        self.selenium.get('%s%s' % (self.live_server_url,
-                                    reverse('crud:export-create')))
-        name_input = self.selenium.find_element_by_id("id_name")
-        name_input.send_keys(name)
-        self.selenium.find_element_by_xpath('//button[@type="submit"]').click()
-        self.assertTrue(Export.objects.latest('id'))
+# Commented as we don't have the use at the moment
+#
+# from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+# from django.urls import reverse
+# from selenium.webdriver.firefox.webdriver import WebDriver
+# from exports.models import Export
+#
+#
+# class MySeleniumTests(StaticLiveServerTestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         super(MySeleniumTests, cls).setUpClass()
+#         cls.selenium = WebDriver()
+#
+#     @classmethod
+#     def tearDownClass(cls):
+#         cls.selenium.quit()
+#         super(MySeleniumTests, cls).tearDownClass()
+#
+#     def test_create(self):
+#         name = 'name1'
+#         self.selenium.get('%s%s' % (self.live_server_url,
+#                                     reverse('crud:export-create')))
+#         name_input = self.selenium.find_element_by_id("id_name")
+#         name_input.send_keys(name)
+#         self.selenium.find_element_by_xpath('//button[@type="submit"]').click()
+#         self.assertTrue(Export.objects.latest('id'))
