a66ce816b5a43946be3059b94c16849674492331
Julien Delasoie <julien.delasoie@epfl.ch>
2018-02-16 14:07:31 +0100

    Add basic UI workflow

--- infoscience_exports/exports/forms.py
+++ infoscience_exports/exports/forms.py
@@ -1,10 +1,17 @@
 from django import forms
+from django.http import HttpResponseRedirect
 
 from log_utils import FormLoggingMixin
-from .models import Export
+from .models import Export, User
 
 
 class ExportForm(FormLoggingMixin, forms.ModelForm):
     class Meta:
         model = Export
-        fields = ['name']
+        exclude = ['user']
+        # fields = ['name',
+        #           'url',
+        #           'bullets_type',
+        #           'bullets_type',
+        #           ]
+
