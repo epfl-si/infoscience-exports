71cd1ed99de1f6f7f6a3734b908368d2353e8401
jdelasoie <julien.delasoie@epfl.ch>
2018-02-20 16:21:03 +0100

    Add simplified bullet and thumbnail settings (#8)

--- infoscience_exports/exports/models/settings.py
+++ infoscience_exports/exports/models/settings.py
@@ -11,33 +11,26 @@ class BaseSettings(models.Model):
         abstract = True
 
 
+class ThumbnailSettings(BaseSettings):
+    show_thumbnail = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
 class BulletsSettings(BaseSettings):
-    BULLETS_TYPE_CHARACTER = 'C'
-    BULLETS_TYPE_NUMBER = 'N'
     BULLETS_TYPE_CHOICE = (
-        (BULLETS_TYPE_CHARACTER, 'Character'),
-        (BULLETS_TYPE_NUMBER, 'Number'),
+        ('', ''),
+        ('CHARACTER_STAR', '*'),
+        ('CHARACTER_MINUS', '-'),
+        ('NUMBER_ASC', '1, 2, 3, ...'),
+        ('NUMBER_DESC', '..., 3, 2, 1'),
     )
 
-    BULLETS_ORDER_ASCENDING = '+'
-    BULLETS_ORDER_DESCENDING = '-'
-    BULLETS_ORDER_CHOICE = (
-        (BULLETS_ORDER_ASCENDING, 'Ascending'),
-        (BULLETS_ORDER_DESCENDING, 'Descending'),
-    )
-
-    bullets_type = models.CharField(max_length=1,
+    bullets_type = models.CharField(max_length=255,
                                     choices=BULLETS_TYPE_CHOICE,
-                                    null=True,
+                                    null=True
                                     )
-    bullets_text = models.CharField(max_length=5,
-                                    null=True,
-                                    )
-    bullets_order = models.CharField(max_length=1,
-                                     choices=BULLETS_ORDER_CHOICE,
-                                     default=BULLETS_ORDER_ASCENDING,
-                                     null=True,
-                                     )
 
     class Meta:
         abstract = True
