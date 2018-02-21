dafcd5acd467825ec20936babe6321ca34c63f55
Julien Delasoie <julien.delasoie@epfl.ch>
2018-02-21 14:19:22 +0100

    Sync staticfiles folder

--- staticfiles/js/export.js
+++ staticfiles/js/export.js
@@ -1,8 +1,11 @@
 jQuery(function($) {
 
 	$('#btn-preview').click(function () {
-        var url = $("#id_url").val();
-        $.get('/preview/', {url: url}, function (data) {
+		var params = {};
+        params['url'] = $("#id_url").val();
+		params['bullet'] = $("#id_bullets_type").find(":selected").val();
+		params['thumb'] = $("#id_show_thumbnail").is(':checked');
+        $.get('/preview/', {params: params}, function (data) {
             ($("#display-mrc21xml").html(data));
         });
     });
