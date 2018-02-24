e246dc237283a3c7197f87710a095a30b6f14041
ebreton <email>
2018-02-24 09:25:32 +0100

    update versions.py

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -1,9 +1,16 @@
+# flake8: noqa
 # This file is autognerated by post-commit hook
 
-_release = '0.1.0-19-ge599098'
-_version = '0.1.1'
+# the release comes from git and should not be modified
+# => read-only
+_release = '0.1.0-24-g1d20363'
+
+# you can set the next version number manually
+# if you do not, the system will make sure that version > release
+# => read-write, >_release
+_version = '0.1.1-rc'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
-_build = 'e599098f1eebefc6d9959912d2ee5f79ae10dde6'
-        
+# => read-only
+_build = '1d203631214de7ba82a1a450bda039177cccdce2'
