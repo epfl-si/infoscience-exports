76f7afda2037a55282d31836413a3ab38dd9a919
ebreton <email>
2018-03-01 10:27:46 +0100

    updated requirements-dev

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.2.2-3-g3536f88'
+_release = '0.2.2-7-g2ff2d8a'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.2.2'
+_version = '0.2.3'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '3536f88f6cc76e05e708c46a5039c9a921422a5f'
+_build = '2ff2d8a8b0ceb5a4614e61bb94c0835efb136638'
