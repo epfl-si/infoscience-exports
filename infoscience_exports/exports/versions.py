25c20f9c79cf4908cd47d6d375a3a5f06f0e8096
ebreton <email>
2018-03-01 07:35:11 +0100

    added command update_release publish to create release on github

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.1.0-59-gf2af8ee'
+_release = '0.2.2-3-g3536f88'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.2.2'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'f2af8ee8e4af0b9edecdd32f024c1818f67de80e'
+_build = '3536f88f6cc76e05e708c46a5039c9a921422a5f'
