d1e4a0df98416142ef39262ea12b1b3a554a0852
ebreton <email>
2018-03-01 22:00:47 +0100

    customize columns displayed in the admin. user+exports

--- infoscience_exports/exports/admin.py
+++ infoscience_exports/exports/admin.py
@@ -11,9 +11,14 @@ from .models import Export, User
 admin.autodiscover()
 admin.site.__class__ = TequilaAdminSite
 
+class EPFLUserModelAdmin(UserAdmin):
+    list_display = ('username', 'email', 'sciper', 'last_login', 'is_superuser')
+
 
 class ExportLoggedModelAdmin(LoggedModelAdminMixin, ModelAdmin):
-    pass
+    list_display = ('name', 'user', 'updated_at')
+    list_filter = ('updated_at', )
+
 
 admin.site.register(Export, ExportLoggedModelAdmin)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, EPFLUserModelAdmin)
