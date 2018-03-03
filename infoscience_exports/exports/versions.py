90cc30c95bfd607bd8ed9b56e358b68434e7faa1
ebreton <email>
2018-03-03 10:05:16 +0100

    fix integration with Tequila

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.3.0-5-gcbb5a38'
+_release = '0.3.0-9-gf6f12ed'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.3.1-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'cbb5a386946062b0b404e884d5166a630a3be871'
+_build = 'f6f12ed4ed59877a57b832d70d0c28aea907fe87'
