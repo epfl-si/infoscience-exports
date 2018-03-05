5312eecab777cc970e37a76181ee486e85a67398
ebreton <email>
2018-03-05 18:30:45 +0100

    udpated make push-prod to ask for confirmation

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.3.1-5-g43fc7ec'
+_release = '0.3.1-6-gb6e4a39'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.3.2-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '43fc7ecccae9888a208a30e5118885d4f18cbb8d'
+_build = 'b6e4a39489a3d235fbc9f27104d361494eddfbe4'
