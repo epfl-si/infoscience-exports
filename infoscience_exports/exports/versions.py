26c62b7297a9e2780020d5b71b45d9f29cdd05f3
ebreton <email>
2018-03-05 21:23:02 +0100

    use SITE_PATH in STATIC/MEDIA_URL settings

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.3.2-5-g77ae43a'
+_release = '0.3.2-7-g21fd635'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.3.3-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '77ae43a6aaca8a106267c8385e270565d052884f'
+_build = '21fd63583c17353b179a6943c3a8906227287123'
