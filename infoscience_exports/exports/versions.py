6e8f135d27815453d0d6504023d4efbc82f4081c
ebreton <email>
2018-03-09 14:53:52 +0100

    merging migrations

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.3.3-15-g578aa23'
+_release = '0.3.3-18-gbe5b6a5'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.3.4-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '578aa2302c8a5fc5f59bd88cfb09e868061df6c3'
+_build = 'be5b6a59a7af49c14978d389b6ec4bf058c9d0e4'
