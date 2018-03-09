e28130b02395b00fadf43379f6b1cc766cedaedb
ebreton <email>
2018-03-09 15:23:44 +0100

    static files served by NGINX

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.3.3-18-gbe5b6a5'
+_release = '0.3.3-29-g5b1fde4'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.3.4'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'be5b6a59a7af49c14978d389b6ec4bf058c9d0e4'
+_build = '5b1fde4eb9cdb69eb1c29cc400a15c5af479a4bd'
