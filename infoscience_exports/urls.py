beabdeb08fd508b08c0490be085c139a6c134730
ebreton <email>
2018-03-12 15:01:41 +0100

    removed staticfiles_urlpatterns

--- infoscience_exports/urls.py
+++ infoscience_exports/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from rest_framework.routers import DefaultRouter
 
@@ -38,5 +37,3 @@ else:
     urlpatterns = app_patterns
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# FIXME: to remove once nginx is intalled on TIND infrastructure
-urlpatterns += staticfiles_urlpatterns()
