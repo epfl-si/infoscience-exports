97af8f67a2199caf413a8c42793ba06f675c1145
ebreton <email>
2018-03-13 20:57:20 +0100

    updated requirements with new pipfile

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.3.5-10-gbd9b71d'
+_release = '0.3.5-11-g04539e2'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.3.5'
+_version = '0.3.6-rc'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'bd9b71d83ff1030c4276811fb34b086bc7c9e2ff'
+_build = '04539e222e3a34ec11842f150f9d2d15fc637e05'
