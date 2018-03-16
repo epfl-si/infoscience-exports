8cd5adfff9afe8fb033d059541a66b0e27862dc3
ebreton <email>
2018-03-16 21:30:45 +0100

    added tests for authors

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.3.7-3-g3a4e66e'
+_release = '0.3.7-14-ge2e540c'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,5 +13,4 @@ _version = '0.3.8-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-
-_build = '086e59f5d9fdf7c643968137b2f1fe3936657d3d'
+_build = 'e2e540c36d6a5228522fec14dc1f7326436fd714'
