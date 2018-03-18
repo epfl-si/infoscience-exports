dfe85281d0e4ead4100968dd6ec1af8d629f5799
ebreton <email>
2018-03-18 10:01:13 +0100

    remove .0 from article.ELA_URL.0

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.3.7-22-g403a888'
+_release = '0.3.7-23-gdd90c13'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.3.8-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '403a888e275409e8c514a2cf53abed28e9826823'
+_build = 'dd90c1315d7a2fd3594a0ae56a1f02ca1bc8760e'
