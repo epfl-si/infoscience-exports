1c614f5d051f5ff2dd62af57d1adbdbd9e1d4509
ebreton <email>
2018-03-21 14:30:11 +0100

    run codecov commands in .travis>script

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.4.1-20-gf920a61'
+_release = '0.4.1-21-g069f6c9'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.4.2-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'f920a611b62d6f121cae83c8b9fe44872f86ec63'
+_build = '069f6c9d85ad9d2f1ff5dc8833095317a05799b9'
