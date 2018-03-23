5f9d07b2be5081a11b60be93803ed333228d0015
dragonleman <lindo.duratti@epfl.ch>
2018-03-23 09:27:31 +0100

    Prepared release 0.4.2

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.4.1-36-g26d815c'
+_release = '0.4.1-38-g67a65d4'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.4.2-rc'
+_version = '0.4.2'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '26d815c235ee1f00f0e8dbd7360a6f8321130a8a'
+_build = '67a65d4bff6d5319cbc0a47eddcf5e072307d97a'
