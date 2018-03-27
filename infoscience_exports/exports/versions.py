9b0a04f9ef5cb7c7b6f8a8b69ae016311210dc10
ebreton <email>
2018-03-27 08:44:00 +0200

    updated locale files .po

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.4.2-8-ge8bd636'
+_release = '0.4.2-9-ga1cca14'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.4.3-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'e8bd6367b8e9ff10116775fb1237542bf48197f3'
+_build = 'a1cca141d3de97354000ba596a4b0e6bb45ed62e'
