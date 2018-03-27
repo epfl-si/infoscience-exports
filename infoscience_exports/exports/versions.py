67629214b9c1b91a1b835e917020f966ee30d4a1
ebreton <email>
2018-03-27 14:35:30 +0200

    correction traductions FR

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.4.2-11-g9b0a04f'
+_release = '0.4.2-12-gacbcbb5'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.4.3-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '9b0a04f9ef5cb7c7b6f8a8b69ae016311210dc10'
+_build = 'acbcbb50420ea59f5e8b86b96d606aa9a19fee9b'
