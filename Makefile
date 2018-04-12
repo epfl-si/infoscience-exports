d8f0b24b9218da5baf268e71af0fe89f6935d519
Julien Delasoie <julien.delasoie@epfl.ch>
2018-04-12 14:26:58 +0200

    Add old to new url csv generator

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@
 	dump restore release push-prod deploy \
 	fast-test test coverage shell \
 	migration-load-dump migration-build-delta \
-	migration-migrate
+	migration-migrate migration-post-generate-csv
 
 VERSION:=$(shell python update_release.py -v)
 
@@ -272,6 +272,11 @@ migration-build-delta:
 
 migration-migrate:
 	docker-compose -f docker-compose-dev.yml exec web python infoscience_exports/manage.py migrate_from_legacy
+
+migration-post-generate-csv:
+	docker-compose -f docker-compose-dev.yml exec web python infoscience_exports/manage.py legacy_url_old_to_new \
+	--jahia_csv_path "/var/log/django/infoscience_exports_new_url_jahia.csv" \
+	--people_csv_path "/var/log/django/infoscience_exports_new_url_people.csv"
 
 check-env:
 ifeq ($(wildcard .env),)
---
+++ infoscience_exports/exporter/management/commands/legacy_url_old_to_new.py
@@ -0,0 +1,40 @@
+import csv
+import logging
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from exports.models import LegacyExport
+
+logger = logging.getLogger('migration')
+
+
+class Command(BaseCommand):
+
+    help = "Write to csv what become of the old url with the new system "
+
+    def add_arguments(self, parser):
+        parser.add_argument('--jahia_csv_path', nargs='+', type=str)
+        parser.add_argument('--people_csv_path', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        with open(options['jahia_csv_path'][0], 'w') as csv_jahia_file:
+            logger.info("Building jahia csv :{}".format(options['jahia_csv_path'][0]))
+            writer = csv.writer(csv_jahia_file)
+            writer.writerow(['old_url', 'new_url'])
+
+            for legacy_export in LegacyExport.objects.filter(origin='JAHIA').select_related('export'):
+                new_url = settings.SITE_DOMAIN + legacy_export.export.get_absolute_url()
+                row = [legacy_export.legacy_url, new_url]
+                writer.writerow(row)
+
+        with open(options['people_csv_path'][0], 'w') as csv_people_file:
+            logger.info(
+                "Building people csv :{}".format(options['people_csv_path'][0]))
+            writer = csv.writer(csv_people_file)
+            writer.writerow(['old_url', 'new_url'])
+
+            for legacy_export in LegacyExport.objects.filter(origin='PEOPLE').select_related('export'):
+                new_url = settings.SITE_DOMAIN + legacy_export.export.get_absolute_url()
+                row = [legacy_export.legacy_url, new_url]
+                writer.writerow(row)
