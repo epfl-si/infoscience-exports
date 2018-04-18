7baabef16c542b57bf1cf562fd4eee7f39090cda
Julien Delasoie <julien.delasoie@epfl.ch>
2018-04-18 17:41:05 +0200

    Fix 2 for user field length for Django-Tequila

--- infoscience_exports/exports/models/user.py
+++ infoscience_exports/exports/models/user.py
@@ -7,17 +7,11 @@ class User(AbstractUser):
     # sciper not necessarily an integer, could contain letters for a service account
     sciper = models.CharField(max_length=100, null=True, blank=True)
     where = models.CharField(max_length=100, null=True, blank=True)
-    units = models.CharField(max_length=300, null=True, blank=True)
-    group = models.CharField(max_length=4096, null=True, blank=True)
+    units = models.TextField(null=True, blank=True)
+    group = models.TextField(null=True, blank=True)
     classe = models.CharField(max_length=100, null=True, blank=True)
     statut = models.CharField(max_length=100, null=True, blank=True)
-    memberof = models.CharField(max_length=1024, null=True, blank=True)
 
-    @property
-    def profile(self):
-        """ Work around current implementation of DjangoTequilla which expects to have all Tequila
-            attributes in a profile object"""
-        return self
 
     def __str__(self):
         if self.email:
