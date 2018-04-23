3b2e0b8bdd7d509a1c79d0409bb18bc176f9a696
Julien Delasoie <julien.delasoie@epfl.ch>
2018-04-23 15:37:09 +0200

    Need update generation is only on EPFL active exports

--- infoscience_exports/exporter/management/commands/generate_need_update_export.py
+++ infoscience_exports/exporter/management/commands/generate_need_update_export.py
@@ -13,25 +13,50 @@ class Command(BaseCommand):
            "jahia list of exports-sciper (provided by Francis, INC0219923)"
 
     def add_arguments(self, parser):
-        parser.add_argument('output', nargs='*', type=str)
-        parser.add_argument('people', nargs='*', type=str)
-        parser.add_argument('jahia', nargs='*', type=str)
+        parser.add_argument('--output_csv_path', nargs='*', type=str)
+        parser.add_argument('--people_csv_path', nargs='*', type=str)
+        parser.add_argument('--jahia_csv_path', nargs='*', type=str)
 
-    def handle(self, output, people, jahia, *args, **options):
-        if not output:
-            raise CommandError("Missing the 'output' argument")
-        if not people:
-            raise CommandError("Missing the 'people' argument")
-        if not jahia:
-            raise CommandError("Missing the 'jahia' argument")
+    def handle(self, output_csv_path, people_csv_path, jahia_csv_path, *args, **options):
+        if not output_csv_path:
+            raise CommandError("Missing the 'output_csv_path' argument")
+        if not people_csv_path:
+            raise CommandError("Missing the 'people_csv_path' argument")
+        if not jahia_csv_path:
+            raise CommandError("Missing the 'jahia_csv_path' argument")
 
-        with open(output[0], 'w') as f:
+        with open(jahia_csv_path[0], 'r') as f:
+            reader = csv.reader(f)
+            jahia_full_list = list(reader)
+            jahia_legacy_exports_ids = []
+
+            for row in jahia_full_list[1:]:
+                jahia_legacy_exports_id = SettingsModel.objects.get_legacy_export_id_from_url(row[8])
+                if jahia_legacy_exports_id:
+                    jahia_legacy_exports_ids.append(jahia_legacy_exports_id)
+
+            self.stdout.write("Jahia ids found {}".format(jahia_legacy_exports_ids))
+
+        with open(people_csv_path[0], 'r') as f:
+            reader = csv.reader(f)
+            people_full_list = list(reader)
+            people_legacy_exports_ids = []
+
+            for row in people_full_list[1:]:
+                people_legacy_exports_id = SettingsModel.objects.get_legacy_export_id_from_url(row[2].strip())
+                if people_legacy_exports_id:
+                    people_legacy_exports_ids.append(people_legacy_exports_id)
+            self.stdout.write(
+                "People ids found {}".format(people_legacy_exports_ids))
+
+        with open(output_csv_path[0], 'w') as f:
             writer = csv.writer(f)
 
             writer.writerow(['legacy id',
                              'legacy url',
                              'new generated url',
                              'number of new elements since migration',
+                             'used in',
                              ])
 
             i = 0
@@ -40,6 +65,18 @@ class Command(BaseCommand):
             for exporter in SettingsModel.objects.all():
                 i = i+1
                 self.stdout.write("Doing n. {}/{}".format(i, total))
+
+                used_in = ''
+
+                if str(exporter.id) in jahia_legacy_exports_ids:
+                    used_in = 'Jahia'
+                elif str(exporter.id) in people_legacy_exports_ids:
+                    used_in = 'People'
+                else:
+                    #ignore
+                    self.stdout.write("Can not found an usage for this url")
+                    continue
+
                 old_url = 'https://infoscience-legacy.epfl.ch/' \
                           'curator/export/{}'.format(exporter.id)
 
@@ -50,7 +87,7 @@ class Command(BaseCommand):
                         'd1y': '2018',
                         'of' : 'xm',
                     }
-                    new_url = exporter.build_advanced_search_url(invenio_vars=invenio_vars, limit=200)
+                    new_url = exporter.build_advanced_search_url(invenio_vars=invenio_vars)
                 except ValueError:
                     self.stdout.write("ignoring this basket url")
                     continue
@@ -77,6 +114,7 @@ class Command(BaseCommand):
                     old_url,
                     new_url,
                     number_of_new_record,
+                    used_in,
                 ]
 
                 writer.writerow(row)
