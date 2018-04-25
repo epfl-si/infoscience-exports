8a6983bf10a9915002e7195033cd5ef1b752d156
Julien Delasoie <julien.delasoie@epfl.ch>
2018-04-25 13:25:31 +0200

    Set new version 0.4.7rc

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -8,7 +8,7 @@ _release = '0.4.7-3-g5ce8962'
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.4.9'
+_version = '0.4.7rc'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
