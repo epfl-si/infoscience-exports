c550fb3379951ad4bdcb52e426933d97d412470d
Julien Delasoie <julien.delasoie@epfl.ch>
2018-04-26 10:44:20 +0200

    Allow to specify if we want to migrate without list ids

--- infoscience_exports/exporter/management/commands/migrate_from_legacy.py
+++ infoscience_exports/exporter/management/commands/migrate_from_legacy.py
@@ -30,21 +30,23 @@ class Command(BaseCommand):
         parser.add_argument('--ids_csv_path', nargs='+', type=str)
         parser.add_argument('--jahia_csv_path', nargs='+', type=str)
         parser.add_argument('--people_csv_path', nargs='+', type=str)
+        parser.add_argument('--no-filter', nargs='+', type=str)
 
     def handle(self, *args, **options):
 
-        if not options.get('ids_csv_path'):
+        if not options.get('ids_csv_path') and not options.get('no-filter'):
             raise CommandError("Missing the 'ids_csv_path' argument")
 
-        # selective mode
         list_ids = []
-        with open(options['ids_csv_path'][0], 'r') as ids_csv_path:
-            reader = csv.reader(ids_csv_path)
-            next(reader)
-            # from l = [[1,2,3],[4,5,6], [7], [8,9]] to
-            # [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            list_ids = list(reader)
-            list_ids = reduce(operator.concat, list_ids)
+        if not options.get('no-filter'):
+            # selective mode
+            with open(options['ids_csv_path'][0], 'r') as ids_csv_path:
+                reader = csv.reader(ids_csv_path)
+                next(reader)
+                # from l = [[1,2,3],[4,5,6], [7], [8,9]] to
+                # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                list_ids = list(reader)
+                list_ids = reduce(operator.concat, list_ids)
 
         SettingsModel.objects.load_exports_from_jahia(
             options['jahia_csv_path'][0],
