8ebe148847576dc8a51ff8ebd82faf6537e5a250
Julien Delasoie <julien.delasoie@epfl.ch>
2018-05-08 10:44:01 +0200

    Prepared release 0.5.5

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.5.2-4-g095765e'
+_release = '0.5.4-7-ga7cad9b'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.5.4'
+_version = '0.5.5'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '095765e26da18a97e931adc7c9059ac61fcba0c0'
+_build = 'a7cad9b4ea549814cba7986ac20130c45b856488'
