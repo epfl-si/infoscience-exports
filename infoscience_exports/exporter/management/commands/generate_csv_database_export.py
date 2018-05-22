585b5b7b142ffecfaa437c943872dfd0dec40a18
Julien Delasoie <julien.delasoie@epfl.ch>
2018-05-22 15:48:27 +0200

    Add a command to generate a csv for all exports

---
+++ infoscience_exports/exporter/management/commands/generate_csv_database_export.py
@@ -0,0 +1,43 @@
+import csv
+
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+from exporter.models import Export
+
+
+class Command(BaseCommand):
+
+    help = "Generate a csv file, where all the useful information are set"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--output', nargs='*', type=str)
+
+    def handle(self, output, *args, **options):
+        if not output:
+            raise CommandError("Missing the 'output' argument")
+
+        with open(output[0], 'w') as f:
+            header = ['id', 'name', 'created_at', 'updated_at', 'absolute_url', 'url', 'legacy_url', 'user']
+
+            for field in Export._meta.fields:
+                if field.name not in header:
+                    header.append(field.name)
+
+            writer = csv.writer(f)
+
+            writer.writerow(header)
+
+            for export in Export.objects.all().select_related('user'):
+                try:
+                    export.legacy_url = 'https://infoscience-legacy.epfl.ch/curator/export/{}/'.format(export.legacyexport_set.all()[0].legacy_id)
+                except IndexError:  # no legacy linked
+                    export.legacy_url = ''
+
+                export.absolute_url = settings.SITE_DOMAIN + export.get_absolute_url()
+                row = []
+
+                for field_name in header:
+                    row.append(getattr(export, field_name))
+
+                writer.writerow(row)
