17a4f60ca804e78b3dcf164ef1f84b0c14d06c0d
Julien Delasoie <julien.delasoie@epfl.ch>
2020-07-22 14:54:21 +0200

    Fix not being able to add a root SITE_PATH

--- infoscience_exports/settings/base.py
+++ infoscience_exports/settings/base.py
@@ -23,7 +23,10 @@ def get_env_variable(var_name, default=None):
 
 parsed_url = parse.urlparse(get_env_variable('SITE_URL'))
 SITE_DOMAIN = "{0}://{1}".format(parsed_url.scheme, parsed_url.netloc)
-SITE_PATH = parsed_url.path.rstrip('/')
+if parsed_url.path == '':
+    SITE_PATH = '/'
+else:
+    SITE_PATH = parsed_url.path.rstrip('/')
 
 # Django Tequila
 AUTHENTICATION_BACKENDS = ('django_tequila.django_backend.TequilaBackend',)
