82600dd7598252fd2310d29c8d0962c962e52587
Julien Delasoie <julien.delasoie@epfl.ch>
2022-04-19 17:34:30 +0200

    Update django-tequila version from the latest pip version

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.5.11-6-gd17f9f6'
+_release = '0.5.11-21-gf3e6c5a'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.5.12-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'd17f9f6f0355c474466f6c0d5e18d7d6f75db888'
+_build = 'f3e6c5a636afc2a4406c4853b67c47e480c7b3bf'
