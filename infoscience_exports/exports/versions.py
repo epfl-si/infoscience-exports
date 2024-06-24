5a49c702fc242447b90c79fc748edf8a23fd487e
Julien Delasoie <julien.delasoie@epfl.ch>
2024-06-24 16:25:05 +0200

    refactor: Locally load css and js, to remove dependencies with infoscience.epfl.ch

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.5.11-57-gc64db90'
+_release = '0.5.11-73-g411c8e0'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.5.12-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'c64db9032fb66860ea80497b175839beacbfac06'
+_build = '411c8e0e37e8a45dd7413b1a73dd48b3ccf7d54a'
