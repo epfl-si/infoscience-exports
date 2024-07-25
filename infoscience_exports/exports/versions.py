d5b11c7e05340dd85e98f66e80239e3a876e9e4d
Julien Delasoie <julien.delasoie@epfl.ch>
2024-07-25 11:53:42 +0200

    fix: Typo

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.5.11-120-g19e06cf'
+_release = '0.5.11-121-g57c959b'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.5.15'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '19e06cfce89cc8f09e70d3a58fb98ec43986c9ea'
+_build = '57c959bf0275a893fb981bd05419205250690c8d'
