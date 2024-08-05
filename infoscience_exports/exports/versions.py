19fc7597af8944394e21507d4bbf7a5c0c5f8d9e
Julien Delasoie <julien.delasoie@epfl.ch>
2024-08-05 15:16:05 +0200

    fix: Remove thumbnail selector, as it is not a current option on this state of the migration

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.5.11-122-gd5b11c7'
+_release = '0.5.11-124-gd1f0cdd'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.5.16'
+_version = '0.5.17'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'd5b11c7e05340dd85e98f66e80239e3a876e9e4d'
+_build = 'd1f0cddc5799a1280ef96a2f7f23daa8e348834c'
