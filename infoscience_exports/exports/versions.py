30a9f6cb7f96697360796fbb492ec5af421849be
Julien Delasoie <julien.delasoie@epfl.ch>
2024-08-15 16:47:05 +0200

    chore: Bump version

--- infoscience_exports/exports/versions.py
+++ infoscience_exports/exports/versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.5.11-124-gd1f0cdd'
+_release = '0.5.11-130-g84932e0'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.5.17'
+_version = '0.5.18'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'd1f0cddc5799a1280ef96a2f7f23daa8e348834c'
+_build = '84932e03093068abe0350fa89d88ec68609f4b4a'
