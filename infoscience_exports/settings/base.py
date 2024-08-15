2c1bad54fa11765dc9ec07d3f69ead206e2ea185
Julien Delasoie <julien.delasoie@epfl.ch>
2024-08-15 14:39:21 +0200

    chore: Set cache from 2h to 12h

--- infoscience_exports/settings/base.py
+++ infoscience_exports/settings/base.py
@@ -223,7 +223,7 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
         'LOCATION': 'generated_export_cache',
-        'TIMEOUT': get_env_variable('CACHE_TIMEOUT', 7200),  # 2h
+        'TIMEOUT': get_env_variable('CACHE_TIMEOUT', 43200),  # 12h
         'OPTIONS': {
             'MAX_ENTRIES': 3000,  # default is 300
             'CULL_FREQUENCY': 5,  # 1/5 of the cache is clean when max entry is reached
