8000b488a6252757b01e7c3921b02c1a45e1c077
Julien Delasoie <julien.delasoie@epfl.ch>
2025-01-17 12:05:25 +0100

    fix: Fix missing cache table when creating a new app

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
+python /usr/src/app/infoscience_exports/manage.py createcachetable
 exec gunicorn -w 3 -b 0.0.0.0:3000 --threads 5 --max-requests 500 --max-requests-jitter 30 --chdir /usr/src/app/infoscience_exports wsgi:application
