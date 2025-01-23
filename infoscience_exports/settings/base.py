6dc6251cd8371f163bb5823cebdfd72bcdc0fe6a
Julien Delasoie <julien.delasoie@epfl.ch>
2025-01-23 11:13:00 +0100

    fix: Fix missing env var loading

--- infoscience_exports/settings/base.py
+++ infoscience_exports/settings/base.py
@@ -29,6 +29,7 @@ SITE_PATH = parsed_url.path.rstrip('/')
 AUTHENTICATION_BACKENDS = ('django_tequila.django_backend.TequilaBackend',)
 TEQUILA_SERVICE_NAME = "Infoscience Exports"
 TEQUILA_SERVER_URL = "https://tequila.epfl.ch"
+TEQUILA_ALLOWED_REQUEST_HOSTS = os.environ.get('TEQUILA_ALLOWED_REQUEST_HOSTS')
 AUTH_USER_MODEL = 'exports.User'
 
 # override django-tequila urls if we are serving the application from a folder path
